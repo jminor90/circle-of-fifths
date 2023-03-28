f1ca1ee77deabd13401f34d3f3b4d544b767e3d6
Joshua <120212106+jminor90@users.noreply.github.com>
2023-03-28 16:54:02 -0700

    Experimental styling, needs some fine tuning though

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,49 @@
-import data from '../assets/data/data.json'
+import './Homepage.css'
 
 export default function Homepage() {
   // console.log("You are a bold one")
   // console.log(data)
 
   return (
-    <h2>Homepage.js</h2>
+    <>
+      <h2>Homepage.js</h2>
+      <div>Hello</div>
+      <div class="god-container">
+        <div class="all-container">
+          <div class="major-container">
+            <div class="major-item">1</div>
+            <div class="major-item">2</div>
+            <div class="major-item">3</div>
+            <div class="major-item">4</div>
+            <div class="major-item">5</div>
+            <div class="major-item">6</div>
+            <div class="major-item">7</div>
+            <div class="major-item">8</div>
+            <div class="major-item">9</div>
+            <div class="major-item">10</div>
+            <div class="major-item">11</div>
+            <div class="major-item">12</div>
+            <div class="major-item">Major</div>
+          </div>
+        </div>
+        <div class="another-container">
+          <div class="minor-container">
+            <div class="minor-item">1</div>
+            <div class="minor-item">2</div>
+            <div class="minor-item">3</div>
+            <div class="minor-item">4</div>
+            <div class="minor-item">5</div>
+            <div class="minor-item">6</div>
+            <div class="minor-item">7</div>
+            <div class="minor-item">8</div>
+            <div class="minor-item">9</div>
+            <div class="minor-item">10</div>
+            <div class="minor-item">11</div>
+            <div class="minor-item">12</div>
+            <div class="minor-item">minor</div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
