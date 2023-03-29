3e600b0b9f5197ef8102bb9c2db7c4473b6038e6
Joshua <120212106+jminor90@users.noreply.github.com>
2023-03-29 16:54:49 -0700

    Updating for Deployment...

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
       <Routes>
         <Route path ="/circle-of-fifths" element={<AppContainer/>}>
           <Route index element={<Homepage />} />
-          <Route path="/circle-of-fifths/CMajor" element={<CMajor />} />
+          <Route path="/CMajor" element={<CMajor />} />
           <Route path="/GMajor" element={<GMajor />} />
           <Route path="/DMajor" element={<DMajor />} />
           <Route path="/AMajor" element={<AMajor />} />
