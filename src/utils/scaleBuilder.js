21e7731233b5ff8210100d2e2a36a7b1362c57db
Joshua <120212106+jminor90@users.noreply.github.com>
2023-04-06 13:43:29 -0700

    Cleaned up code to be more readable

--- src/utils/scaleBuilder.js
+++ src/utils/scaleBuilder.js
@@ -1,28 +1,22 @@
 export default function scaleBuilder (keyVar, accNum, accExpr) {
   // Takes in the Key and removes # or b from it so we can build a scale
-  var key = keyVar;
+  let key = keyVar;
   key = key.replace(/[^A-Z\.]+/g, "");
 
-  console.log("The Key Given...")
-  console.log(key)
-
-  //All scales are alphabetical; this loop will start building a scale starting with the Key given and loop around to give 7 notes in scale order
+  //All scales are alphabetical; this loop will start building a scale starting with the Key given
+  //and loop around to give 7 notes in scale order
   const list = ["A", "B", "C", "D", "E", "F", "G"];
   let i = list.indexOf(key);
   let newList = [];
   for (let counter = 0; counter < 7; ++counter) {
     newList.push(list[i]);
-    // console.log(`${list[i]}`);
     i = (i + 1) % list.length;
   }
-  console.log("newList Scale before edit")
-  console.log(newList)
-  
-  
-  
-  //AccidentalExpression 1 = Sharp, 2 = Flat, 0 = Natural
 
+  //AccidentalExpression 1 = Sharp, 2 = Flat, 0 = Natural
   switch (accExpr) {
+    
+    // Sharps
     case 1:
       // Order of sharps will always remain the same, this takes in how many sharps given for that scale
       let sharpCount = accNum
@@ -35,16 +29,12 @@ export default function scaleBuilder (keyVar, accNum, accExpr) {
       for (let i=0; i < sharpCount; i++) {
         newList.splice(newList.indexOf(sharpResult[i].replace(/[^A-Z\.]+/g, "")),1,sharpResult[i])
       }
-  
-      console.log("Result/ Sharps")
-      console.log(sharpResult)
-      console.log("newList Scale after edit")
-      console.log(newList)
 
       return(
         newList
       )
-
+    
+    // Flats
     case 2:
       let flatCount = accNum
       
@@ -55,16 +45,12 @@ export default function scaleBuilder (keyVar, accNum, accExpr) {
       for (let i=0; i < flatCount; i++) {
         newList.splice(newList.indexOf(flatResult[i].replace(/[^A-Z\.]+/g, "")),1,flatResult[i])
       }
-      
-      console.log("Result/ Flats")
-      console.log(flatResult)
-      console.log("newList Scale after edit")
-      console.log(newList)
-      
+
       return(
         newList
       )
-
+    
+    // All Natural
     default:
       return(
         newList
@@ -72,7 +58,6 @@ export default function scaleBuilder (keyVar, accNum, accExpr) {
       
   }
 
-  
 }
 
 // scaleBuilder("F", 1, 2)
